a6b98c5cb4439ad8f977d38666f22ede1d1110a4
Kishan <kishanpradhan226@gmail.com>
2023-05-18 21:22:40 +0530

    Last Updated on 18.05.2023

---
+++ LeetCode/Recursion/[C++] [Recursion+Backtracking] 46. Permutations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    void solve(vector<int> n, vector<vector<int>> &ans, int index){
+        if(index>=n.size())
+        {
+            ans.push_back(n);
+            return;
+        }
+        for(int i=index; i<n.size();i++){
+            swap(n[i], n[index]);
+            solve(n, ans, index+1);
+            //backtrack
+            swap(n[i], n[index]);
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int index=0;
+        solve(nums, ans, index);
+        return ans;
+    }
+};
