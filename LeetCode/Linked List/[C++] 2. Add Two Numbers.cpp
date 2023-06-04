972ce216468facdbd77be9a10833eb5a059e74d9
Kishan <kishanpradhan226@gmail.com>
2023-06-04 18:52:24 +0530

    Last Updated on 04.06.2023

---
+++ LeetCode/Linked List/[C++] 2. Add Two Numbers.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *ans = new ListNode(0);
+        ListNode* temp = ans;
+        int carry = 0;
+        while(l1 != NULL || l2 != NULL || carry)
+        {
+            int sum = 0;
+            if(l1 != NULL)
+            {
+                sum += l1->val;
+                l1 = l1->next; 
+            }
+            if(l2 != NULL)
+            {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            sum+=carry;
+            carry = sum/10;
+            ListNode *nodeToInsert = new ListNode(sum%10);
+            temp->next = nodeToInsert;
+            temp = temp->next;
+        }
+        ListNode *nodeToDelete = ans;
+        ans = ans->next;
+        delete(nodeToDelete);
+        return ans;
+        
+    }
+};
