128e5e5867f60718c00a705fa48c4c40c635feaa
Kishan <kishanpradhan226@gmail.com>
2023-07-09 20:36:10 +0530

    Last Updated on 09.07.2023

--- Heap/[C++] Representation of Heap.cpp
+++ Heap/[C++] Representation of Heap.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 // MAX HEAP
@@ -114,6 +114,26 @@ void heapify(int arr[], int n, int i){
     
 }
 
+/*
+-> Swap arr[1] and arr[n]
+-> Place root node to it's correct place
+
+T.C -> O(nlog(n))
+*/
+void heapSort(int arr[], int n){
+    int t = n;
+    
+    while(t > 1)
+    {
+        //Step 1
+        swap(arr[1], arr[t]);
+        t--;
+        
+        //Step 2
+        heapify(arr, t, 1);
+    }
+}
+
 int main()
 {
     heap h;
@@ -140,5 +160,47 @@ int main()
         cout<< a[i] <<" ";
     }
     cout<<endl;
+
+    //Heap Sort
+    heapSort(a, n);
+    
+    cout<<"After Heap Sorting the max heap, Printing it: "<<endl;
+    for(int i=1;i<=n;i++){
+        cout<< a[i] <<" ";
+    }
+    cout<<endl<<endl;
+    // ** To apply Heap Sort on Normal Array, firstly convert the array into max heap then apply heap sort algorithm.  **
+    
+    //STL
+    //Using Priority Queue here
+    
+    //max heap
+    priority_queue<int> pq;
+    
+    pq.push(4);
+    pq.push(2);
+    pq.push(3);
+    pq.push(5);
+    pq.push(1);
+    cout<<"Maximum element at top: "<<pq.top()<<endl;
+    pq.pop();
+    cout<<"Maximum element at top: "<<pq.top()<<endl;
+    cout<<"Size is: "<<pq.size()<<endl;
+    cout<<endl<<endl;
+    
+    
+    //min heap
+    priority_queue<int, vector<int>, greater<int>> minheap;
+    
+    minheap.push(4);
+    minheap.push(2);
+    minheap.push(3);
+    minheap.push(5);
+    minheap.push(1);
+    cout<<"Minimum element at top: "<<minheap.top()<<endl;
+    minheap.pop();
+    cout<<"Minimum element at top: "<<minheap.top()<<endl;
+    minheap.pop();
+    cout<<"Size is: "<<minheap.size()<<endl;
     return 0;
 }
