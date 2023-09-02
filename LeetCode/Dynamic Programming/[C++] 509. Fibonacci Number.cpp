85def4830a503d13f58fbe3846ff73bfe62f424f
Kishan <kishanpradhan226@gmail.com>
2023-09-02 20:36:12 +0530

    Last Updated on 02.09.2023

--- LeetCode/Dynamic Programming/[C++] 509. Fibonacci Number.cpp
+++ LeetCode/Dynamic Programming/[C++] 509. Fibonacci Number.cpp
@@ -20,3 +20,51 @@ public:
         return solve(n, dp);
     }
 };
+
+
+//Bottom-Up Approach [Tabulation]
+
+/*
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0 || n == 1)
+            return n;
+        vector<int> dp(n+1);
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i=2;i<=n;i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+};
+*/
+
+
+//DP + Space Optimization
+
+/*
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0 || n == 1)
+            return n;
+        
+        int prev1 = 1;
+        int prev2 = 0;
+
+        for(int i=2;i<=n;i++)
+        {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+};
+*/
