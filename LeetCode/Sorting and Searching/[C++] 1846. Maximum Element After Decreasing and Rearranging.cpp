80489114a343c8e880b7052f9dbe4e7169c2494d
Kishan <kishanpradhan226@gmail.com>
2023-11-15 12:13:25 +0530

    Last Updated on 15.11.2023

--- LeetCode/Sorting and Searching/[C++] 1846. Maximum Element After Decreasing and Rearranging.cpp
+++ LeetCode/Sorting and Searching/[C++] 1846. Maximum Element After Decreasing and Rearranging.cpp
@@ -1,3 +1,38 @@
+// O(n)
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> count(n+1, 0);
+        for(int x : arr) {
+            count[min(x, n)]++;
+        }
+        
+        int maxEl = 1;
+        int i = 0;
+        int num = 1;
+
+        while(num <= n && i < n) {
+            int freq = count[num];
+
+            maxEl = max(maxEl, num);
+            i += (freq == 0) ? 1 : freq;
+            num++;
+        }
+
+
+        //Edge Case - When 'num' doesn't consider element/elements greater than maxEl (when i exceeds n and some elements are still left to be considered).
+        if(i>n)
+            for(int i=maxEl+1;i<n+1;++i)
+                if(count[i]) maxEl++;
+        
+        return maxEl;
+
+    }
+};
+
+
+// O(n^2)
 class Solution {
 public:
     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
