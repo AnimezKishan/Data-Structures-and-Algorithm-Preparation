e5500d3c51adea8f82aa2e71d7fe1f71aa30e139
Kishan <kishanpradhan226@gmail.com>
2023-12-09 10:33:39 +0530

    Last Updated on 09.12.2023

--- LeetCode/Binary Tree/[C++] [Morris Traversal] 94. Binary Tree Inorder Traversal.cpp
+++ LeetCode/Binary Tree/[C++] [Morris Traversal] 94. Binary Tree Inorder Traversal.cpp
@@ -71,3 +71,28 @@ public:
         return ans;
     }
 };
+
+
+//Brute Force
+/*
+class Solution {
+public:
+    vector<int> ans;
+    void solve(TreeNode* root){
+        if(!root)
+            return;
+        
+        if(root->left)
+            solve(root->left);
+        
+        ans.push_back(root->val);
+
+        if(root->right)
+            solve(root->right);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        solve(root);
+        return ans;
+    }
+};
+*/
