0178ceac9370bc7d8ae33b0f37264f3be5fc7276
Kishan <kishanpradhan226@gmail.com>
2024-02-09 13:03:40 +0530

    Last Updated on 09.02.2024

---
+++ LeetCode/Dynamic Programming/[C++] 368. Largest Divisible Subset.cpp
@@ -0,0 +1,34 @@
+// LIS Varient
+
+class Solution {
+public:
+    vector<int> ans;
+    int dp[1001];
+    int n;
+    void solve(int i, vector<int> &nums, int prev,vector<int> temp){
+        if(i >= n){
+            if(temp.size() > ans.size())
+                ans = temp;
+            return;
+        }
+
+        if((int)temp.size() > dp[i] && nums[i] % prev == 0){
+            dp[i] = temp.size();
+            temp.push_back(nums[i]);
+            //take
+            solve(i+1, nums, nums[i], temp);
+            temp.pop_back();
+        }
+
+        //don't take
+        solve(i+1, nums, prev, temp);
+
+    }
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        n = nums.size();
+        memset(dp, -1, sizeof(dp));
+        sort(begin(nums), end(nums));
+        solve(0, nums, 1,{});
+        return ans;
+    }
+};
