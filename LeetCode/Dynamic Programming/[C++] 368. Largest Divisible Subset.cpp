1519df2880d05e3c4c5a1803c0651874eeda3abd
Kishan <kishanpradhan226@gmail.com>
2024-02-09 13:13:02 +0530

    Last Updated on 09.02.2024

--- LeetCode/Dynamic Programming/[C++] 368. Largest Divisible Subset.cpp
+++ LeetCode/Dynamic Programming/[C++] 368. Largest Divisible Subset.cpp
@@ -32,3 +32,36 @@ public:
         return ans;
     }
 };
+
+// Recursion (gives TLE)
+/*
+class Solution {
+public:
+    vector<int> ans;
+    int n;
+    void solve(int i, vector<int> &nums, int prev,vector<int> temp){
+        if(i >= n){
+            if(temp.size() > ans.size())
+                ans = temp;
+            return;
+        }
+
+        if(temp.empty() || nums[i] % temp.back() == 0){
+            temp.push_back(nums[i]);
+            //take
+            solve(i+1, nums, nums[i], temp);
+            temp.pop_back();
+        }
+
+        //don't take
+        solve(i+1, nums, prev, temp);
+
+    }
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        n = nums.size();
+        sort(begin(nums), end(nums));
+        solve(0, nums, 1, {});
+        return ans;
+    }
+};
+*/
