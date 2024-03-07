55b8d1ec764b54e461141149cd401451b8317006
Kishan <kishanpradhan226@gmail.com>
2024-03-07 12:17:43 +0530

    Last Updated on 07.03.2024

--- LeetCode/Linked List/876. Middle of the Linked List/[C++] [Optimized] 876. Middle of the Linked List.cpp
+++ LeetCode/Linked List/876. Middle of the Linked List/[C++] [Optimized] 876. Middle of the Linked List.cpp
@@ -38,3 +38,23 @@ public:
         return getMiddle(head);
     }
 };
+
+/*
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        while(fast->next){
+            fast = fast->next;
+            slow = slow->next;
+
+            if(fast->next)
+                fast = fast->next;
+        }
+
+        return slow;
+    }
+};
+*/
