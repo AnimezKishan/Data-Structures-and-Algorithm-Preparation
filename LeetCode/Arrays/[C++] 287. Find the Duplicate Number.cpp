91d542faa651ebaff86c8212150415f6168fbf20
Kishan <kishanpradhan226@gmail.com>
2024-03-24 17:29:19 +0530

    Last Updated on 24.03.2024

--- LeetCode/Arrays/[C++] 287. Find the Duplicate Number.cpp
+++ LeetCode/Arrays/[C++] 287. Find the Duplicate Number.cpp
@@ -1,13 +1,13 @@
-/*
-Fast Slow Approach (Two Pointer)
-The key is to understand how to treat the input array as a linked list.
+// Floyd's Cycle Detection
+// The key is to understand how to treat the input array as a linked list.
 
+/*
 Take the array [1,3,4,2] as an example, the index of this array is [0,1,2,3], we can map the index to the nums[n].
 0 → 1 → 3 → 2 → 4 → 3 → 2
 
 If there are a repeated numbers in the array, take the array [1,3,4,2,2] as an example,
 
-0 → 1 → 3 → 2 → 4 → 3 → 2 → 4 → 2 → 4 → 2 .......
+0 → 1 → 3 → 2 → 4 → 2 → 4 → 2 → 4 → 2 .......
 
 Here 2 → 4 creates a cycle so we can apply fast slow approach here.
 */
@@ -15,19 +15,21 @@ Here 2 → 4 creates a cycle so we can apply fast slow approach here.
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int slow = 0;
-        int fast = 0;
+        int slow = 0, fast = 0;
 
+        // Detect Cycle
+        // since fast and slow are equal at start, so using do while.
         do{
             slow = nums[slow];
             fast = nums[nums[fast]];
         }while(slow != fast);
 
+        // Now, find which element is causing cycle.
         slow = 0;
-        do{
+        while(slow != fast){
             slow = nums[slow];
             fast = nums[fast];
-        }while(slow != fast);
+        }
 
         return slow;
     }
