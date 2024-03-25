31fb12a862a1e99d67fc6f1e375bec8f7ebdc3bc
Kishan <kishanpradhan226@gmail.com>
2024-03-25 11:25:41 +0530

    Last Updated on 25.03.2024

--- LeetCode/Arrays/[C++] 287. Find the Duplicate Number.cpp
+++ LeetCode/Arrays/[C++] 287. Find the Duplicate Number.cpp
@@ -34,3 +34,23 @@ public:
         return slow;
     }
 };
+
+
+// Similar Solution to 442
+
+/*
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++){
+            int idx = abs(nums[i]);
+            if(nums[idx] > 0)
+                nums[idx] = -nums[idx];
+            else
+                return idx;
+        }
+
+        return -1;
+    }
+};
+*/
