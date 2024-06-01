50a067fed872bde558ed44da04939c54d05b64f6
Kishan <kishanpradhan226@gmail.com>
2024-06-01 08:51:12 +0530

    Last Updated on 01.06.2024

---
+++ LeetCode/Arrays/[C++] [String] 3110. Score of a String.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int ans = 0;
+
+        for(int i=0;i<s.length()-1;i++)
+            ans += abs(int(s[i]) - int(s[i+1]));
+
+        return ans;
+    }
+};
