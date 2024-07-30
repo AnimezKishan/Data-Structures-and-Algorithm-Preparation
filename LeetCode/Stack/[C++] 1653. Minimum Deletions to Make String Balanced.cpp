b7b183ba15eb0291ded4c7e6d0a26e053d3bd150
Kishan <kishanpradhan226@gmail.com>
2024-07-30 19:41:39 +0530

    Last Updated on 30.07.2024

--- LeetCode/Stack/[C++] 1653. Minimum Deletions to Make String Balanced.cpp
+++ LeetCode/Stack/[C++] 1653. Minimum Deletions to Make String Balanced.cpp
@@ -18,3 +18,36 @@ public:
         return ans;
     }
 };
+
+
+// Approach II
+/* 
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int n = s.length();
+
+        // keep account of no. of 'a' appreaing on right side for each index.
+        int countA = 0;
+        vector<int> rightA(n);
+        for(int i=n-1; i>=0; i--){
+            rightA[i] = countA;
+            if(s[i] == 'a')
+                countA++;
+        }
+
+        // pick a index where, no. of 'b' left to it + no. of 'a' right to it is minimum.
+        int ans = INT_MAX;
+        // On the way, keep account of no. of 'b' appreaing on left side for each index.
+        int countB = 0;
+        for(int i=0; i<n; i++){
+            ans = min(ans, countB + rightA[i]);
+            if(s[i] == 'b')
+                countB++;
+        }
+
+        return ans;
+
+    }
+};
+*/
