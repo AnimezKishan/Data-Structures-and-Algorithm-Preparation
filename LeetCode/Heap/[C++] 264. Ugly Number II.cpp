284b58f8f401121fc8471b726d5a4c1d24548b88
Kishan <kishanpradhan226@gmail.com>
2024-08-18 15:04:45 +0530

    Last Updated on 18.08.2024

--- LeetCode/Heap/[C++] 264. Ugly Number II.cpp
+++ LeetCode/Heap/[C++] 264. Ugly Number II.cpp
@@ -26,3 +26,41 @@ public:
         return num;
     }
 };
+
+// DP
+/*
+class Solution {
+public:
+    unordered_map<int, bool> mp;
+    bool isUgly(int n) {
+        if(n <= 0)
+            return false;
+        if(n == 1)
+            return true;
+        
+        if(mp.find(n) != mp.end())
+            return mp[n];
+        
+        if(n%2 == 0 && isUgly(n/2))
+            return mp[n] = true;
+        else if(n%3 == 0 && isUgly(n/3))
+            return mp[n] = true;
+        else if(n%5 == 0 && isUgly(n/5))
+            return mp[n] = true;
+        
+        return mp[n] = false;
+    }
+    int nthUglyNumber(int n) {
+        mp.clear();
+        int num = 1;
+        while(n > 0) {
+            if(isUgly(num)) {
+                n--;
+            }
+            num++;
+        }
+        
+        return num-1;
+    }
+};
+*/
