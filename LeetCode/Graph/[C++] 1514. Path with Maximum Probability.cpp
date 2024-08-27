0271300b4108003a4df31af9670de9f83e0b2bd9
Kishan <kishanpradhan226@gmail.com>
2024-08-27 12:14:47 +0530

    Last Updated on 27.08.2024

--- LeetCode/Graph/[C++] 1514. Path with Maximum Probability.cpp
+++ LeetCode/Graph/[C++] 1514. Path with Maximum Probability.cpp
@@ -38,3 +38,46 @@ public:
         return ans[end_node];
     }
 };
+
+
+// Dijkstra's Algorithm
+/*
+class Solution {
+public:
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
+        unordered_map<int, vector<pair<int, double>>> adj;
+        for(int i=0; i<edges.size(); i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            double w = succProb[i];
+
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w});
+        }
+
+        vector<double> distance(n, 0);
+        distance[start_node] = 1.0;
+
+        // max-heap
+        priority_queue<pair<double, int>> pq;
+        pq.push({1.0, start_node});
+        while(!pq.empty()){
+            int u = pq.top().second;
+            double w = pq.top().first;
+            pq.pop();
+
+            for(auto &nbr : adj[u]){
+                int v = nbr.first;
+                double dist = nbr.second;
+
+                if(w * dist > distance[v]){
+                    distance[v] = w * dist;
+                    pq.push({w * dist, v});
+                }
+            }
+        }
+
+        return distance[end_node];
+    }
+};
+*/
