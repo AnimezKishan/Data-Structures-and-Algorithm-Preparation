b93382bb9cc97363a4dc954a2450046455cceee7
Kishan <kishanpradhan226@gmail.com>
2024-09-23 20:04:54 +0530

    Last Updated on 23.09.2024

--- LeetCode/Dynamic Programming/[C++] 2707. Extra Characters in a String.cpp
+++ LeetCode/Dynamic Programming/[C++] 2707. Extra Characters in a String.cpp
@@ -36,3 +36,37 @@ public:
         return solve(0, s, dp);
     }
 };
+
+
+/*
+class Solution {
+public:
+    int dp[51];
+    int solve(int idx, string &s, unordered_set<string> &st, int &n) {
+        if(idx >= n)
+            return 0;
+
+        if(dp[idx] != -1)
+            return dp[idx];
+
+        //skip the current character
+        int result = 1 + solve(idx+1, s, st, n);
+
+        // take current character and try to find substring
+        for(int i=idx; i<n; i++){
+            string curr = s.substr(idx, i-idx+1);
+            if(st.count(curr))
+                result = min(result, solve(i+1, s, st, n));
+        }
+
+        return dp[idx] = result;
+    }
+    int minExtraChar(string s, vector<string>& dictionary) {
+        int n = s.length();
+        memset(dp, -1, sizeof(dp));
+        unordered_set<string> st(begin(dictionary), end(dictionary));
+
+        return solve(0, s, st, n);
+    }
+};
+*/
