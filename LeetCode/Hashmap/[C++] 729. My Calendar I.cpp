4852eb644e77482aa824ce4618692687b18d45ac
Kishan <kishanpradhan226@gmail.com>
2024-09-27 21:29:05 +0530

    Last Updated on 27.09.2024

--- LeetCode/Hashmap/[C++] 729. My Calendar I.cpp
+++ LeetCode/Hashmap/[C++] 729. My Calendar I.cpp
@@ -28,6 +28,41 @@ public:
     }
 };
 
+
+// Approach II
+/*
+Interval Based
+~ For interval1 (start1, end1) and interval2 (start2, end2) to be overlapping
+    -> max(start1, start2) < min(end1, end2)
+*/
+/*
+class MyCalendar {
+public:
+    vector<pair<int, int>> bookings;
+    MyCalendar() {
+        
+    }
+
+    bool checkOverlapping(int start1, int end1, int start2, int end2) {
+        if(max(start1, start2) < min(end1, end2))
+            return true;
+        return false;
+    }
+    
+    bool book(int start, int end) {
+        // check if double booking happening or not
+        for(pair<int, int> region : bookings) {
+            if(checkOverlapping(region.first, region.second, start, end))
+                return false;
+        }
+
+        bookings.push_back({start, end});
+        return true;
+
+    }
+};
+*/
+
 /**
  * Your MyCalendar object will be instantiated and called as such:
  * MyCalendar* obj = new MyCalendar();
