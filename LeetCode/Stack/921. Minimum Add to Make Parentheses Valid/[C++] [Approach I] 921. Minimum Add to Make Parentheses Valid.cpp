d7644ac2e37774dfd266812ed74dad0d8c8f4ad4
Kishan <kishanpradhan226@gmail.com>
2024-10-09 20:46:24 +0530

    Last Updated on 09.10.2024

--- LeetCode/Stack/921. Minimum Add to Make Parentheses Valid/[C++] [Approach I] 921. Minimum Add to Make Parentheses Valid.cpp
+++ LeetCode/Stack/921. Minimum Add to Make Parentheses Valid/[C++] [Approach I] 921. Minimum Add to Make Parentheses Valid.cpp
@@ -25,3 +25,21 @@ public:
         return cnt;
     }
 };
+
+// more easily using stack
+/*
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char> st;
+        for(char ch : s){
+            if(ch == ')' && !st.empty() && st.top() == '(')
+                st.pop();
+            else
+                st.push(ch);
+        }
+
+        return st.size();
+    }
+};
+*/
