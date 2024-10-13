7ea7314e44da948d1064490fb45d3293fd8b59d1
Kishan <kishanpradhan226@gmail.com>
2024-10-13 13:36:50 +0530

    Last Updated on 13.10.2024

--- LeetCode/Heap/[C++] 632. Smallest Range Covering Elements from K Lists.cpp
+++ LeetCode/Heap/[C++] 632. Smallest Range Covering Elements from K Lists.cpp
@@ -12,68 +12,61 @@
 T.C -> O(n*k log(k))
 S.C -> O(k)
 */
+// custom DS to store the value and from which row and column it belongs to.
 class Node{
     public:
-    int data;
-    int i;
-    int j;
+    int data, i, j;
 
-    Node(int d, int row, int col)
-    {
-        this->data = d;
-        this->i = row;
-        this->j = col;
+    Node(int data, int i, int j) {
+        this->data = data;
+        this->i = i;
+        this->j = j;
     }
 };
-
-class compare{
-    public:
-    bool operator()(Node* a, Node* b){
-        return a->data > b->data;
-    }
-};
-
 class Solution {
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
-        int mini = INT_MAX;
-        int maxi = INT_MIN;
-        priority_queue<Node*, vector<Node*>, compare> min_heap;
-        int k = nums.size();
+        // comparator for replicate min heap behaviour for our custom DS
+        auto lambda = [&](Node *a, Node *b) {
+            return a->data > b->data;
+        };
 
-        //Insert first elements of all k lists inside min heap.
-        for(int i=0;i<k;i++)
-        {
-            int element = nums[i][0];
-            mini = min(mini, element);
-            maxi = max(maxi, element);
-            min_heap.push(new Node(element, i, 0)); 
+        int mini = INT_MAX, maxi = INT_MIN, k = nums.size();
+        priority_queue<Node*, vector<Node*>, decltype(lambda)> pq(lambda);
+
+        // insert first elements of K lists into min heap
+        for(int i=0; i<k; i++){
+            int elem = nums[i][0];
+            mini = min(mini, elem);
+            maxi = max(maxi, elem);
+
+            pq.push(new Node(elem, i, 0));
         }
-        
-        int start = mini, end = maxi;
-        while(!min_heap.empty())
-        {
-            //Update mini
-            Node *temp = min_heap.top();
+
+        int ansMin = mini, ansMax = maxi;
+        while(!pq.empty()) {
+            // update mini
+            Node *temp = pq.top();
             mini = temp->data;
-            min_heap.pop();
+            pq.pop();
 
-            //Update Range.
-            if((maxi-mini) < (end-start))
-            {
-                start = mini;
-                end = maxi;
+            // check for range, if any smaller range found then update the ans
+            if((maxi - mini) < (ansMax - ansMin)) {
+                ansMax = maxi;
+                ansMin = mini;
             }
 
-            //Update maxi and push next element in min heap.
-            if(temp->j+1 < nums[temp->i].size())
-            {
+            // for the current min value, check for next element in it's list
+            if(temp->j+1 < nums[temp->i].size()){
+                // check for max 
                 maxi = max(maxi, nums[temp->i][temp->j+1]);
-                min_heap.push(new Node(nums[temp->i][temp->j+1], temp->i, temp->j+1));
+                // push next element into min heap
+                pq.push(new Node(nums[temp->i][temp->j+1], temp->i, temp->j+1));
             }
-            else
+            else // if any of the list goes out of bound as we need all k list to check for range
                 break;
         }
-        return {start, end};
+
+        return {ansMin, ansMax};
     }
 };
