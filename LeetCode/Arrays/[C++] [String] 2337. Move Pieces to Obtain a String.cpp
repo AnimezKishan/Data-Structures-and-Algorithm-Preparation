27be0c429b94b9bb2e6e82f95c52f611393d7c1b
Kishan <kishanpradhan226@gmail.com>
2024-12-05 23:00:09 +0530

    Last Updated on 05.12.2024

---
+++ LeetCode/Arrays/[C++] [String] 2337. Move Pieces to Obtain a String.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool canChange(string start, string target) {
+        int n = start.length();
+        int i = 0, j = 0;
+
+        while(i<=n || j<=n) {
+            while(i<n && start[i] == '_')
+                i++;
+            
+            while(j<n && target[j] == '_')
+                j++;
+
+            if(i == n || j == n)
+                return i == n && j == n;
+            
+            if(start[i] != target[j])
+                return false;
+            
+            if(start[i] == 'L' && i<j)
+                return false;
+            
+            if(start[i] == 'R' && i>j)
+                return false;
+
+            i++;
+            j++;
+        }
+
+        return true;
+    } 
+};
