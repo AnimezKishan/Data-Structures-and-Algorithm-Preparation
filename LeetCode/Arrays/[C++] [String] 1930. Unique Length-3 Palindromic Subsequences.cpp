36193306d6a6eeccaf9c47e8e01e90835acdcdd8
Kishan <kishanpradhan226@gmail.com>
2025-01-04 23:31:02 +0530

    Last Updated on 04.01.2025

--- LeetCode/Arrays/[C++] [String] 1930. Unique Length-3 Palindromic Subsequences.cpp
+++ LeetCode/Arrays/[C++] [String] 1930. Unique Length-3 Palindromic Subsequences.cpp
@@ -3,17 +3,18 @@ public:
     int countPalindromicSubsequence(string s) {
         unordered_set<char> letters;
 
-        //store all unique letters
+        // 1. store all unique letters
         for(auto ch:s)
             letters.insert(ch);
         
         int ans = 0;
+        // For each unique letter, repeat step 2 and 3
         for(auto letter : letters){
 
             int left_index = -1;
             int right_index = -1;
 
-            //find first and last occurance of letter
+            // 2. find first and last occurance of letter
             for(int i=0;i<s.length();i++){
                 if(s[i] == letter){
                     if(left_index == -1)
@@ -24,8 +25,8 @@ public:
             }
 
             unordered_set<char> unique_mid;
-            // all unique characters in between of letter's first and last occurance, contribute to 
-            for(int k = left_index+1;k<right_index;k++)
+            // 3. all unique characters in between of letter's first and last occurance, each contribute to forming a new palindromic subsequence of length 3.
+            for(int k = left_index+1; k<right_index; k++)
                 unique_mid.insert(s[k]);
                 
             ans += unique_mid.size();
