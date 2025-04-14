af6326fd9efb7c830cfa4875b3f952a437d17009
Kishan <kishanpradhan226@gmail.com>
2025-04-14 18:53:30 +0530

    Last Updated on 14.04.2025

---
+++ LeetCode/Arrays/[C++] 1534. Count Good Triplets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    public:
+        int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+            unordered_map<int, int> mp;
+            int n = arr.size();
+    
+            int ans = 0;
+            for(int i=0; i<=n-3; i++) {
+                for(int j=i+1; j<=n-2; j++) {
+                    for(int k=j+1; k<n; k++) {
+                        int c1 = abs(arr[i] - arr[j]);
+                        int c2 = abs(arr[j] - arr[k]);
+                        int c3 = abs(arr[i] - arr[k]);
+    
+                        if(c1 <= a && c2 <= b && c3 <= c)
+                            ans++;
+                    }
+                }
+            }
+    
+            return ans;
+        }
+    };
