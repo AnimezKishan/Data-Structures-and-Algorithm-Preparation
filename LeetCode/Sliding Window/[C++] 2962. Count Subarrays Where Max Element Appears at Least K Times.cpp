42964678f0d1ad841b64c302e4f89c44fc53c02e
Kishan <kishanpradhan226@gmail.com>
2025-04-29 12:14:01 +0530

    Last Updated on 29.04.2025

--- LeetCode/Sliding Window/[C++] 2962. Count Subarrays Where Max Element Appears at Least K Times.cpp
+++ LeetCode/Sliding Window/[C++] 2962. Count Subarrays Where Max Element Appears at Least K Times.cpp
@@ -33,3 +33,32 @@ public:
         return ans;
     }
 };
+
+// Other Apporach using sliding window only
+class Solution {
+    public:
+        long long countSubarrays(vector<int>& nums, int k) {
+            int maxi = *max_element(begin(nums), end(nums));
+    
+            int n = nums.size(), i = 0, j = 0, count = 0;
+            long long ans = 0;
+    
+            while(j < n) {
+                if(nums[j] == maxi)
+                    count++;
+                
+                while(count == k) {
+                    ans += n-j;
+    
+                    if(nums[i] == maxi)
+                        count--;
+                    
+                    i++;
+                }
+    
+                j++;
+            }
+    
+            return ans;
+        }
+    };
