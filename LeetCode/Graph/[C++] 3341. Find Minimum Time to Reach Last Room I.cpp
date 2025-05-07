6d1664300140bc1792af792940fa2368ce8f8e89
Kishan <kishanpradhan226@gmail.com>
2025-05-07 21:34:54 +0530

    Last Updated on 07.05.2025

---
+++ LeetCode/Graph/[C++] 3341. Find Minimum Time to Reach Last Room I.cpp
@@ -0,0 +1,47 @@
+// Dijkstra's Algorithm
+
+class Solution {
+public:
+    typedef pair<int, int> P;
+    vector<vector<int>> dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+    bool isSafe(int i, int j, int m, int n) {
+        if(i >= 0 && i < m && j>=0 && j < n)
+            return true;
+        return false;
+    }
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        int m = moveTime.size(), n = moveTime[0].size();
+
+        priority_queue<pair<int, P>, vector<pair<int, P>>, greater<pair<int, P>>> pq; // min-heap
+        vector<vector<int>> minTime(m, vector<int>(n, INT_MAX));
+        pq.push({0, {0, 0}});
+
+        while(!pq.empty()) {
+            int time = pq.top().first;
+            int i = pq.top().second.first;
+            int j = pq.top().second.second;
+            pq.pop();
+
+            for(vector<int> dir : dirs) {
+                int newI = i + dir[0];
+                int newJ = j + dir[1];
+
+                if(isSafe(newI, newJ, m, n)) {
+                    int newTime;
+                    if(time < moveTime[newI][newJ]) {
+                        newTime = moveTime[newI][newJ] + 1;
+                    } else {
+                        newTime = time + 1;
+                    }
+
+                    if(newTime < minTime[newI][newJ]) {
+                        minTime[newI][newJ] = newTime;
+                        pq.push({newTime, {newI, newJ}});
+                    }
+                }
+            }
+        }
+
+        return minTime[m-1][n-1];
+    }
+};
