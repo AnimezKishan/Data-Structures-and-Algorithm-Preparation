be0f5690443b3e34f3fd74f9e7c04e26e1bec01c
Kishan <kishanpradhan226@gmail.com>
2025-05-11 19:58:35 +0530

    Last Updated on 11.05.2025

--- LeetCode/Arrays/[C++] 1550. Three Consecutive Odds.cpp
+++ LeetCode/Arrays/[C++] 1550. Three Consecutive Odds.cpp
@@ -12,3 +12,27 @@ public:
         return false;
     }
 };
+
+
+// Approach II - Counting
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int n = arr.size();
+
+        int count = 0;
+        for(int i=0; i<n; i++) {
+            if(arr[i]&1) {
+                count++;
+
+                if(count == 3)
+                    return true;
+            }
+            else {
+                count = 0;
+            }
+        }
+        
+        return false;
+    }
+};
