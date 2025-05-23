8cf013093ee7a33b4412d80a81e55654e08f672e
Kishan Kumar <91653112+AnimezKishan@users.noreply.github.com>
2025-05-23 22:51:37 +0530

    Last Updated on 23.05.2025

--- LeetCode/Binary Tree/[C++] 3068. Find the Maximum Sum of Node Values.cpp
+++ LeetCode/Binary Tree/[C++] 3068. Find the Maximum Sum of Node Values.cpp
@@ -5,13 +5,10 @@ public:
     typedef long long ll;
     long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
         ll sum = 0;
-        
         // count of changes made should be of even no. of times
         int count = 0;
-
         // If, count comes odd, which node should be taken such that ans would be maximum. 
         ll mini = INT_MAX;
-
         for(ll num : nums){
             if((num ^ k) > num){
                 sum += (num ^ k);
